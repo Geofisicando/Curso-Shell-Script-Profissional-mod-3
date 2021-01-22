542b8f93e01b40c5466422729d858b95f701db18
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-01-22 20:45:47 -0300

    Dicas de debug

---
+++ exemplos/dicas/README.md
@@ -0,0 +1,6 @@
+#### Dicas para uma função debug profissional
+
+* Gravar logging
+* Cor diferente por nível
+* Mudar o alinhamento da mensagem
+* Fazer pré-processamento para verificar outras condições além de DEBUG=1
---
+++ exemplos/dicas/exemplo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DEBUG=${1:-0}
+
+debug(){
+	[ "$1" -le "$DEBUG" ] || return
+	local prefixo
+	local nivel
+	nivel="$1"
+	case "$1" in
+		1) prefixo="---{ ";;
+		2) prefixo="------{ ";;
+		3) prefixo="---------{ ";;
+		4) prefixo="------------{ ";;
+		*) echo "Mensagem não categorizada: $*"; return;;
+	esac
+	shift
+
+	echo "($nivel)$prefixo$*"
+}
+
+debug 1 "***Início do programa***"
+
+read -p "Digite seu nome: " NOME
+
+[ -z "$NOME" ] && debug 3 "Usuário não digitou um nome!"
+
+echo "O nome digitado é $NOME"
+
+read -p "Qual a sua idade, $NOME? " IDADE
+
+[ -z "$IDADE" ] && debug 3 "Usuário não digitou uma idade!"
+
+echo "Sua idade é $IDADE"
+
+echo "contando 5 segundos..."
+debug 2 "Entrando no laço..."
+
+for i in $(seq 5)
+do
+	sleep 1
+	debug 4 "$i"
+done
+
+debug 1 "***Fim do programa***"
