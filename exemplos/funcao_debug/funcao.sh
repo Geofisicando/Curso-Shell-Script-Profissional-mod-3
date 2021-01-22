80e92e5c290413a658b599b7111d263840d457a2
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-01-22 19:59:01 -0300

    Função de debug

---
+++ exemplos/funcao_debug/funcao.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#DEBUG="1"
+
+debug(){
+	MARCADOR="---{"
+	[ "$DEBUG" == "1" ] && echo -e "\033[33;1m$MARCADOR $*\033[m"
+}
+
+debug "***Início do programa***"
+debug "Modo de depuração ativado DEBUG=1"
+
+read -p "Digite seu nome: " NOME
+
+[ -z "$NOME" ] && debug "Usuário não digitou nenhum nome!"
+
+echo "O nome digitado é $NOME"
+
+read -p "Qual a sua idade, $NOME? " IDADE
+
+[ -z "$NOME" ] && debug "Usuário não digitou nenhuma idade!"
+
+echo "Sua idade é $IDADE"
+
+debug "$(date)"
+debug "***Fim da execução***"
